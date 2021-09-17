86551e769234e2ef2a971d6e13db06eb76d5f779
Daniel Polo <od.polo@gmail.com>
2021-09-17 00:09:02 -0500

    feat: Use mapbox features to not render polygons

--- components/Map.js
+++ components/Map.js
@@ -1,30 +1,30 @@
 import mapboxgl from 'mapbox-gl';
 import React, { useEffect, useState } from 'react';
-import useLayer from '../hooks/useLayer';
-import useOpportunities from '../hooks/useOpportunities';
 
 mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_PUBLIC_TOKEN;
 
 const CANCUN_COORDINATES = [-86.879, 21.1427];
 const opportunities = ['jobs_w', 'empress', 'clinics', 'escuels'];
+const timeSteps = [15, 30, 60];
+const mediums = ['car', 'bike', 'walk'];
 
 const convertToGeoJSON = (features) => ({
   type: 'FeatureCollection',
   features,
 });
 
-const createLayer = (map, features, property, max) => {
-  map.addSource(property, {
+const createLayer = (id, map, features, property, max, visibility) => {
+  map.addSource(id, {
     type: 'geojson',
     data: convertToGeoJSON(features), // 'https://docs.mapbox.com/mapbox-gl-js/assets/ne_110m_admin_1_states_provinces_shp.geojson'
   });
   map.addLayer({
-    id: property,
+    id,
     type: 'fill',
-    source: property,
+    source: id,
     filter: ['>', ['get', property], 0],
     layout: {
-      visibility: 'none',
+      visibility,
     },
     paint: {
       'fill-color': [
@@ -49,6 +49,7 @@ function Map({ city, data }) {
   const features = Object.values(data);
   const [map, setMap] = useState(null);
   const [layerId, setLayerId] = useState();
+  const [hexagonId, setHexagonId] = useState();
 
   useEffect(() => {
     const mapInstance = new mapboxgl.Map({
@@ -61,43 +62,60 @@ function Map({ city, data }) {
     mapInstance.on('load', () => {
       opportunities.forEach((opportunity) => {
         const values = features.map((item) => item.properties[opportunity]);
-        createLayer(mapInstance, features, opportunity, Math.max(...values));
+        createLayer(opportunity, mapInstance, features, opportunity, Math.max(...values), 'none');
       });
     });
 
     setMap(mapInstance);
   }, []);
 
-  // useEffect(() => {
-  //   if (map && displayJobs) {
-  //     map.on('load', () => {
-  //       displayJobs();
-  //     });
-  //   }
-  // }, [map, displayJobs]);
-
   useEffect(() => {
     if (map && layerId) {
-      console.log(layerId);
       map.on('click', layerId, async (e) => {
-        const response = await fetch(`/api/cities/${city}/features/${e.features[0].properties.h3_ddrs}`);
+        const featureId = e.features[0].properties.h3_ddrs;
+
+        // Clear previoyus isochrone
+        if (hexagonId) {
+          mediums.forEach((medium) => {
+            timeSteps.forEach((timeStep) => {
+              map.removeLayer(`${hexagonId}-${medium}-${timeStep}`);
+            });
+          });
+        }
+
+        // Fetch data
+        const response = await fetch(`/api/cities/${city}/features/${featureId}`);
         const text = await response.text();
         const json = JSON.parse(text);
-        const walkIds = Object.keys(json).filter((id) => json[id][1] < 60);
-        const walk = walkIds.map((id) => ({
-          ...data[id],
-          properties: {
-            ...data[id].properties,
-            walk_time: json[id][1],
-          },
-        }));
-        console.log(walk);
-        const [set] = createLayer(map, walk, 'walk_time', Math.max(...walk.map((item) => item.properties.walk_time)));
-        set();
-        // const { walk, bike, car } = getLayers(data, 15);
+
+        // Create 9 isochrone variant layers
+        mediums.forEach((medium, i) => {
+          timeSteps.forEach((timeStep) => {
+            const filteredIds = Object.keys(json).filter((id) => json[id][i] < timeStep);
+            const hexagons = filteredIds.map((id) => ({
+              ...data[id],
+              properties: {
+                ...data[id].properties,
+                [medium]: json[id][1],
+              },
+            }));
+            const max = Math.max(...hexagons.map((item) => item.properties[medium]));
+            createLayer(`${featureId}-${medium}-${timeStep}`, map, hexagons, medium, max, 'none');
+          });
+        });
+        // Hide all opportunity layers
+        opportunities.forEach((opportunity) => {
+          map.setLayoutProperty(opportunity, 'visibility', 'none');
+        });
+        // Show default isochrone
+        map.setLayoutProperty(`${featureId}-car-60`, 'visibility', 'visible');
+
+        // Set active IDS
+        setLayerId(`${featureId}-car-60`);
+        setHexagonId(featureId);
       });
     }
-  }, [map, layerId]);
+  }, [map, layerId, hexagonId]);
 
   const handleSelectChange = (event) => {
     const nextOpportunity = event.currentTarget.value;
