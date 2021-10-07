a9993123d17a2e04adafad8cd65a252f246c632f
Daniel Polo <od.polo@gmail.com>
2021-10-06 22:22:10 -0500

    refactor(api): remove API endpoints.

--- pages/api/cities/[name]/index.js
+++
@@ -1,9 +0,0 @@
-const handler = async (req, res) => {
-  const { name } = req.query
-  const response = await fetch(`https://calculadora-de-accesibilidad.s3.us-west-2.amazonaws.com/data/${name}/main.json`)
-  const data = await response.text()
-  res.status(200).json(data)
-}
-
-
-export default handler
