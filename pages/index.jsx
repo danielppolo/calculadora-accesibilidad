ed9a38bd883d1fb48982c3b19be757560887a8b7
Daniel Polo <od.polo@gmail.com>
2021-12-16 21:33:18 -0600

    feat: landing page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container, Grid } from '@mui/material';
 import Link from 'next/link';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const Button = () => (
   <Link href="/map">
@@ -9,32 +10,47 @@ const Button = () => (
 );
 
 export default function Home() {
+  const [expanded, setExpanded] = useState(false);
   return (
     <div>
-      <div className="bg-blue h-16 text-white flex items-center justify-between px-16">
+      <div className="bg-blue h-16 px-4 text-white flex items-center justify-between relative md:px-16">
         <img className="h-6" src="/logo-ideamos-blanco.png" alt="Logotipo" />
-        <div className="space-x-8 text-2xl">
+        <div id="desktop-menu" className="space-x-8 text-2xl hidden md:block">
           <a className="hover:opacity-70 duration-100" href="https://ideamos.mx/el-programa">El programa</a>
           <a className="hover:opacity-70 duration-100" href="https://ideamos.mx/indice-de-pilotos">Los pilotos</a>
           <a className="hover:opacity-70 duration-100" href="https://ideamos.mx/noticias">Noticias</a>
           <a className="hover:opacity-70 duration-100" href="https://ideamos.mx/convocatoria">Convocatoria</a>
         </div>
-      </div>
-      <div className="bg-blue h-16 text-white flex items-center justify-between px-16">
-        <div>
-          <p>Una iniciativa de:</p>
-          <p>Con apoyo de:</p>
+        <MenuIcon className="text-white" onClick={() => setExpanded(!expanded)} />
+        <div id="mobile-menu" className={`absolute top-16 left-0 right-0 bg-blue opacity-90 duration-300 overflow-hidden ${expanded ? 'h-64' : 'h-0'}`}>
+          <a className="duration-100 flex items-center text-2xl p-2 font-bold h-16 border-b border-white" href="https://ideamos.mx/el-programa">El programa</a>
+          <a className="duration-100 flex items-center text-2xl p-2 font-bold h-16 border-b border-white" href="https://ideamos.mx/indice-de-pilotos">Los pilotos</a>
+          <a className="duration-100 flex items-center text-2xl p-2 font-bold h-16 border-b border-white" href="https://ideamos.mx/noticias">Noticias</a>
+          <a className="duration-100 flex items-center text-2xl p-2 font-bold h-16" href="https://ideamos.mx/convocatoria">Convocatoria</a>
         </div>
-        <div className="">
-          <a href="https://github.com/itdp">
-            <img className="h-8" src="/github.png" alt="Github Logo" />
+      </div>
+      <div className="bg-blue h-32 px-4 text-white flex items-center justify-between md:px-16 md:h-16">
+        <div className="md:items-center md:space-x-16 md:flex">
+          <div className="space-x-8 flex items-center h-12">
+            <p className="font-bold text-xl">Una iniciativa de:</p>
+            <img className="h-8" src="/itdp.png" alt="ITDP Logo" />
+          </div>
+          <div className="space-x-8 flex items-center h-12">
+            <p className="font-bold text-xl">Con apoyo de:</p>
+            <img className="h-8" src="/bid.png" alt="BID Logo" />
+            <img className="h-8" src="/bid-lab.png" alt="BID LAB Logo" />
+          </div>
+        </div>
+        <div className="hidden md:block">
+          <a href="https://github.com/ITDPmx/calculadora-accesibilidad">
+            <img className="h-6" src="/github.png" alt="Github Logo" />
           </a>
         </div>
       </div>
       <Container maxWidth="md">
         <div className="my-12">
           <div className="my-8">
-            <h1 className="text-center text-3xl font-bold">
+            <h1 className="text-center text-3xl mb-2 font-bold">
               Visualizador de accesibilidad urbana y acceso a oportunidades
             </h1>
             <h2 className="text-center text-xl">
@@ -57,10 +73,10 @@ export default function Home() {
           </div>
         </div>
 
-        <Grid container spacing={3}>
+        <Grid container spacing={3} className="my-8">
           <Grid item xs={12} lg={3}>
-            <img src="" alt="" />
-            <p>
+            <img className="bg-yellow h-48 mb-8" src="" alt="Img" />
+            <p className="text-sm">
               Las ciudades podrán identificar
               zonas de baja accesibilidad a
               oportunidades y brechas de acceso
@@ -74,8 +90,8 @@ export default function Home() {
             </p>
           </Grid>
           <Grid item xs={12} lg={3}>
-            <img src="" alt="" />
-            <p>
+            <img className="bg-yellow h-48 mb-8" src="" alt="Img" />
+            <p className="text-sm">
               Las empresas de la red de
               transporte (ERT) podrán explorar
               zonas con poca oferta de movilidad
@@ -88,8 +104,8 @@ export default function Home() {
             </p>
           </Grid>
           <Grid item xs={12} lg={3}>
-            <img src="" alt="" />
-            <p>
+            <img className="bg-yellow h-48 mb-8" src="" alt="Img" />
+            <p className="text-sm">
               Las y los estudiantes, docentes e
               investigadores que realizan análisis
               sobre movilidad, urbanismo y
@@ -100,8 +116,8 @@ export default function Home() {
             </p>
           </Grid>
           <Grid item xs={12} lg={3}>
-            <img src="" alt="" />
-            <p>
+            <img className="bg-yellow h-48 mb-8" src="" alt="Img" />
+            <p className="text-sm">
               La ciudadanía podrá conocer las
               medidas de accesibilidad y
               oportunidades de áreas de interés
@@ -110,30 +126,32 @@ export default function Home() {
           </Grid>
         </Grid>
 
-        <Grid container spacing={3}>
+        <Grid container spacing={4} className="my-16">
           <Grid item xs={12} lg={6}>
-            <img src="" alt="" />
+            <img className="bg-yellow h-full" src="" alt="" />
           </Grid>
           <Grid item xs={12} lg={6}>
-            <h3>
+            <h3 className="font-bold text-xl mb-6">
               Diseño de soluciones considerando el acceso
               a oportunidades:
             </h3>
-            <p>
+            <p className="text-sm">
               La accesibilidad urbana nos indica a cuántas oportunidades (empleos,
               empresas, clínicas, servicios básicos, etc) se puede acceder en un tiempo y
               medio de transporte dados.
-
+            </p>
+            <br />
+            <p className="text-sm">
               Esta plataforma integra distintos mapas que nos muestran la accesibilidad
               urbana por áreas geográficas o hexágonos de 20 ciudades mexicanas:
             </p>
-            <ul>
-              <li>
+            <ul className="list-inside list-disc space-y-4">
+              <li className="text-sm">
                 Mapa de accesibilidad urbana por ciudad, con indicadores desagregados
                 por género e índice de marginación.
               </li>
-              <li>Mapa de acceso a oportunidades por ciudad.</li>
-              <li>
+              <li className="text-sm">Mapa de acceso a oportunidades por ciudad.</li>
+              <li className="text-sm">
                 Mapa con áreas geográficas desagregado por modo de transporte y
                 tiempo de traslado.
               </li>
@@ -143,29 +161,40 @@ export default function Home() {
 
         <Grid container spacing={3}>
           <Grid item xs={12}>
-            <img src="" alt="" />
+            <img className="bg-yellow h-96" src="" alt="" />
           </Grid>
         </Grid>
 
-        <Grid container spacing={3}>
+        <Grid container spacing={3} className="my-8">
           <Grid item xs={12} lg={6}>
-            <h3>
+            <h3 className="font-bold text-xl mb-6">
               Explorar la plataforma es muy sencillo.
-            </h3>
-            <p>
               También, puedes agendar un taller para aprovechar todo su potencial:
-            </p>
+            </h3>
             <ol>
-              <li>
+              <li className="mb-2 text-sm">
+                <strong>Paso 1.</strong>
+                {' '}
                 Da click en un hexágono + bullet explicativo
               </li>
-              <li>Selecciona el tiempo + bullet</li>
-              <li>
+              <li className="mb-2 text-sm">
+                <strong>Paso 2.</strong>
+                {' '}
+                Selecciona el tiempo + bullet
+
+              </li>
+              <li className="mb-2 text-sm">
+                <strong>Paso 3.</strong>
+                {' '}
                 Selecciona modo de transporte.
               </li>
-              <li>Observa la isocrona y el histograma.</li>
+              <li className="mb-2 text-sm">
+                <strong>Paso 4.</strong>
+                {' '}
+                Observa la isocrona y el histograma.
+              </li>
             </ol>
-            <p>
+            <p className="mb-12 mt-8 text-sm">
               Escribe a ideamos@itdp.org para agendar un taller, asesoría o aclarar
               cualquier duda que no esté incluida en nuestra sección de Preguntas
               Frecuentes.
@@ -174,57 +203,64 @@ export default function Home() {
             <Button />
           </Grid>
           <Grid item xs={12} lg={6}>
-            <img src="" alt="" />
+            <img className="bg-yellow h-full" src="" alt="" />
           </Grid>
         </Grid>
 
-        <Grid container spacing={3}>
+        <Grid container spacing={3} className="my-12">
           <Grid item xs={12}>
-            <img src="" alt="" />
+            <img className="bg-yellow h-96" src="" alt="" />
           </Grid>
         </Grid>
 
-        <Grid container spacing={3}>
+        <Grid container spacing={3} className="my-12 text-sm">
           <Grid item xs={12}>
-            <h3>Preguntas frequentes</h3>
-            <p className="font-bold">Pregunta: Mi ciudad no está en la plataforma ¿qué puedo hacer?</p>
-            <p>
-              Respuesta: Puedes contactarnos si quieres explorar los análisis para una ciudad que no se encuentra en la
-              plataforma, puedes contactarnos.
-            </p>
-
-            <p className="font-bold">Pregunta: ¿Cómo puedo citar la información de la plataforma?</p>
-            <p>
-              Respuesta: Puedes contactarnos si quieres explorar los análisis para una ciudad que no se encuentra en la
-              plataforma, puedes contactarnos.
-            </p>
-
-            <p className="font-bold">Pregunta: ¿Puedo usar la herramienta para mis proyectos?</p>
-            <p>
-              Respuesta: Sí, la herramienta es de código abierto y está publicada en un repositorio de Github. Puedes modificar,distribuir y
-              adaptar, siempre que le reconozca al ITDP la creación original.
-            </p>
-            <p className="font-bold">Pregunta: ¡Tengo más preguntas!</p>
-            <p>Respuesta: Envíanos un correo a ideamos@itdp.org.</p>
-            <p className="font-bold">Pregunta: ¿Dónde puedo descargar la información?</p>
-            <p>
-              Respuesta: La información de accesibilidad y acceso a oportunidades puede descargarse dentro de la plataforma a través del
-              botón “Descargar”. Por el momento no es posible descargar todos los datos al mismo tiempo, pero si lo necesitas ponte en
-              contacto con nosotros.
-            </p>
+            <h3 className="font-bold text-xl mb-6 text-center">Preguntas frequentes</h3>
+            <div className="space-y-4">
+              <div>
+                <p className="font-bold">Pregunta: Mi ciudad no está en la plataforma ¿qué puedo hacer?</p>
+                <p>
+                  Respuesta: Puedes contactarnos si quieres explorar los análisis para una ciudad que no se encuentra en la
+                  plataforma, puedes contactarnos.
+                </p>
+              </div>
+              <div>
+                <p className="font-bold">Pregunta: ¿Cómo puedo citar la información de la plataforma?</p>
+                <p>
+                  Respuesta: Puedes contactarnos si quieres explorar los análisis para una ciudad que no se encuentra en la
+                  plataforma, puedes contactarnos.
+                </p>
+              </div>
+              <div>
+                <p className="font-bold">Pregunta: ¿Puedo usar la herramienta para mis proyectos?</p>
+                <p>
+                  Respuesta: Sí, la herramienta es de código abierto y está publicada en un repositorio de Github. Puedes modificar,distribuir y
+                  adaptar, siempre que le reconozca al ITDP la creación original.
+                </p>
+              </div>
+              <div>
+                <p className="font-bold">Pregunta: ¡Tengo más preguntas!</p>
+                <p>Respuesta: Envíanos un correo a ideamos@itdp.org.</p>
+              </div>
+              <div>
+                <p className="font-bold">Pregunta: ¿Dónde puedo descargar la información?</p>
+                <p>
+                  Respuesta: La información de accesibilidad y acceso a oportunidades puede descargarse dentro de la plataforma a través del
+                  botón “Descargar”. Por el momento no es posible descargar todos los datos al mismo tiempo, pero si lo necesitas ponte en
+                  contacto con nosotros.
+                </p>
+              </div>
+            </div>
           </Grid>
         </Grid>
-
       </Container>
+      <div className="my-32" />
       <div className="bg-black h-16 text-white flex items-center justify-between px-16">
-        <div>
-          <p>Una iniciativa de:</p>
-          <p>Con apoyo de:</p>
-        </div>
-        <div className="">
-          <a href="https://github.com/itdp">
-            <img className="h-8" src="/github.png" alt="Github Logo" />
-          </a>
+        <div className="space-x-4 flex items-center">
+          <img className="h-5" src="/itdp.png" alt="ITDP Logo" />
+          <img className="h-3" src="/logo-ideamos-blanco.png" alt="Logotipo" />
+          <img className="h-5" src="/bid.png" alt="BID Logo" />
+          <img className="h-5" src="/bid-lab.png" alt="BID LAB Logo" />
         </div>
       </div>
     </div>
