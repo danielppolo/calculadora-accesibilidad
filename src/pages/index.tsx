5db0860258bb2a818f086d7bc035f71570aed9f9
Daniel Polo <od.polo@gmail.com>
2023-01-06 02:25:15 -0600

    feat: add hidden input for netlify

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -321,6 +321,7 @@ export default function Home() {
           </Grid>
         </div>
         <form name="contact" method="POST" data-netlify="true">
+          <input type="hidden" name="form-name" value="contact" />
           <p>
             <label>
               Your Name: <input type="text" name="name" />
