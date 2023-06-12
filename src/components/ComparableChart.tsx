573c662ef2c8ed45d9a4414d7807ef95eaff836f
Daniel Polo <od.polo@gmail.com>
2023-06-12 08:59:58 -0600

    feat: add chart with totals

--- src/components/ComparableChart.tsx
+++ src/components/ComparableChart.tsx
@@ -112,7 +112,7 @@ function ComparableChart() {
 
     let instance: Chart | null = null;
     const chartContainer = document.getElementById(
-      'comparable'
+      'comparable-percentage'
     ) as HTMLCanvasElement;
 
     const data = {
@@ -178,9 +178,161 @@ function ComparableChart() {
     intl,
   ]);
 
+  useEffect(() => {
+    const comparableFilter = getComparableFilter(currentVisualization);
+    const comparingCodes =
+      comparableFilter?.code && current?.filters
+        ? [current.filters[comparableFilter.code]].flat()
+        : [];
+
+    const sampleId =
+      comparingCodes.length &&
+      comparableFilter &&
+      generateVariantId({
+        ...current,
+        filters: {
+          ...current.filters,
+          [comparableFilter.code]: comparingCodes[0],
+        },
+      });
+
+    if (
+      isEmpty(comparingGeojson) ||
+      !isComparable(currentVisualization) ||
+      !comparingCodes?.length ||
+      !comparableFilter ||
+      !sampleId ||
+      !comparingGeojson[sampleId] ||
+      !grid
+    ) {
+      return undefined;
+    }
+
+    const comparableKeys =
+      currentVisualization?.comparableOptions?.map((option) => option?.code) ??
+      [];
+    const comparableLabels =
+      currentVisualization?.comparableOptions?.map((option) => option?.name) ??
+      [];
+
+    const cityTotals: Record<string, number> = {};
+    Object.values(grid).forEach((feature) => {
+      comparableKeys.forEach((comparableKey) => {
+        if (cityTotals[comparableKey]) {
+          cityTotals[comparableKey] +=
+            feature?.properties?.[comparableKey] ?? 0;
+        } else {
+          cityTotals[comparableKey] = feature?.properties?.[comparableKey] ?? 0;
+        }
+      });
+    });
+
+    const datasets = comparingCodes.map((optionCode) => {
+      const id = generateVariantId({
+        ...current,
+        filters: {
+          ...current.filters,
+          [comparableFilter.code]: optionCode,
+        },
+      });
+
+      const total = comparableKeys.map((comparableKey) => {
+        return Math.round(
+          comparingGeojson[id]?.features.reduce(
+            (acc: number, feature: Feature<Polygon>) => {
+              return acc + feature.properties?.[comparableKey];
+            },
+            0
+          )
+        );
+      });
+
+      const colors = Object.keys(comparableKeys).map(
+        (key) =>
+          comparableFilter?.options.find((option) => option.code === optionCode)
+            ?.color
+      );
+
+      return {
+        label: comparableFilter?.options?.find(
+          (option) => option.code === optionCode
+        )?.name,
+        data: total,
+        backgroundColor: colors,
+      };
+    });
+
+    let instance: Chart | null = null;
+    const chartContainer = document.getElementById(
+      'comparable-number'
+    ) as HTMLCanvasElement;
+
+    const data = {
+      labels: comparableLabels,
+      datasets,
+    };
+
+    instance = new Chart(chartContainer, {
+      type: 'bar',
+      data,
+      options: {
+        plugins: {
+          legend: {
+            display: false,
+          },
+          title: {
+            display: false,
+            text: intl.formatMessage({
+              defaultMessage: 'Oportunidades alcanzadas',
+              id: 'iR0iQ+',
+            }), // TODO: Remove hardcoded text
+            align: 'start',
+            // font: {
+            //   weight: 'regular',
+            // },
+          },
+          // tooltip: {
+          //   callbacks: {
+          //     label: (context) => {
+          //       let label = context.dataset.label || '';
+          //       if (label) label += '%';
+          //       return label;
+          //     },
+          //   },
+          // },
+        },
+        scales: {
+          y: {
+            beginAtZero: true,
+            display: true,
+            // max: 100,
+          },
+        },
+        responsive: true,
+        maintainAspectRatio: false,
+      },
+    });
+
+    return () => {
+      instance?.destroy();
+    };
+  }, [
+    comparingGeojson,
+    current,
+    current.filters,
+    currentVisualization,
+    grid,
+    intl,
+  ]);
+
   return (
-    <div className="w-full relative h-[200px]">
-      <canvas id="comparable" />
+    <div>
+      <div className="w-full relative h-[200px]">
+        <canvas id="comparable-percentage" />
+      </div>
+      <div className="w-full relative h-[200px]">
+        <canvas id="comparable-number" />
+      </div>
     </div>
   );
 }
