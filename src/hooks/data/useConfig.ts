6da0e071f679ace459f107777f06ae823e8030d4
Daniel Polo <od.polo@gmail.com>
2023-09-06 01:02:03 -0600

    feat: set demo config

--- src/hooks/data/useConfig.ts
+++ src/hooks/data/useConfig.ts
@@ -1,18 +1,17 @@
 import { City, Config } from 'src/types';
-import getConfig from 'src/adapters/contentful/getConfig';
 import { useQuery } from '@tanstack/react-query';
 import queries from 'src/utils/queries';
 import { useMapboxTilesetManager } from 'src/context/mapboxTilesetManager';
 import { useMapboxLayerManager } from 'src/context/mapboxLayerManager';
 import { BASE_LAYER_ID, DEFAULT_LOCALE } from 'src/constants';
 import { useRouter } from 'next/router';
+import remoteConfig from 'src/adapters/config';
 
 type CustomConfig = Config & {
   citiesDictionary: Record<City['code'], City>;
 };
 
 const fetchConfig = async (locale: string) => {
-  const remoteConfig = await getConfig(locale);
   const nextConfig: CustomConfig = {
     ...remoteConfig,
     citiesDictionary: {},
